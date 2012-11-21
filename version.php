b556bed5d9539e4189411f02de77be3a6cb97f2b
Goran Josic <goran.josic@usi.ch>
2012-11-21 15:09:33 +0100

    version bump

--- version.php
+++ version.php
@@ -1,5 +1,5 @@
 <?php
-$plugin->version  = 2012042439;
+$plugin->version  = 2012042440;
                                   
 $plugin->requires = 2010112400;  // Requires this Moodle version - at least 2.0
 $plugin->cron     = 0;
